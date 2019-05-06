ca1751cd5a38e7d74b490c396fb4bd5922d9197e
yvanvds <yvan@attr-x.net>
2019-05-06 22:11:14 +0300

    Added Test Logger

---
+++ AbstractAccountApi/TestLogger.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractAccountApi
+{
+    public class TestLogger : ILog
+    {
+        public void AddError(string message)
+        {
+            Debug.WriteLine("Error: "+ message);
+        }
+
+        public void AddMessage(string message)
+        {
+            Debug.WriteLine("Warning: " + message);
+        }
+    }
+}
